10dcc3e43ab848e6b019de87004e2d1567027f8e
rosicleiamendes15 <149099389+rosicleiamendes15@users.noreply.github.com>
2025-08-04 11:52:17 -0300

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector (".caixa-principal");
+
+const caixaPerguntas = document.querySelector (".caixa-principal");
+
+const caixaAlternativas = document.querySelector (".caixa-principal");
+
+const caixaResultado = document.querySelector (".caixa-principal");
+
+const caixaResultado = document.querySelector (".caixa-principal");
+
+const lista = [ ];
+const perguntas [
+    {
+        enunciado: "perguntas"
+        Alternativa:
+        "Alternativa 1",
+        "Alternativa 2"
+
+    ]
+    }
+
+    
+
+
