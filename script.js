4da5a8f5ee533b2f598cf434a247b70ac3996e11
rosicleiamendes15 <149099389+rosicleiamendes15@users.noreply.github.com>
2025-08-11 11:37:26 -0300

    .

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const textoResultado = document.querySelector('.texto-Resultado');
 
 const lista = [];
 
-const perguntas = 
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
