a13dcf0d153fa9d12d0e34c46d2ef3716451f3dc
rosicleiamendes15 <149099389+rosicleiamendes15@users.noreply.github.com>
2025-08-11 08:19:28 -0300

    .

--- script.js
+++ script.js
@@ -11,14 +11,41 @@ const caixaResultado = document.querySelector (".caixa-principal");
 const lista = [ ];
 const perguntas [
     {
-        enunciado: "perguntas"
-        Alternativa:
-        "Alternativa 1",
-        "Alternativa 2"
-
-    ]
+        enunciado: "perguntas1"
+        Alternativa:["Alternativa 1","Alternativa 2"]
     }
 
-    
+ {
+        enunciado: "perguntas2"
+        Alternativa:["Alternativa 1","Alternativa 2"]
+    }
+
+     {
+        enunciado: "perguntas3"
+        Alternativa:["Alternativa 1","Alternativa 2"]
+    }
+
+     {
+        enunciado: "perguntas4"
+        Alternativa:["Alternativa 1","Alternativa 2"]
+    }
+
+     {
+        enunciado: "perguntas5"
+        Alternativa:["Alternativa 1","Alternativa 2"]
+    }
+
+];
+
+let atual = 0
+let perguntasAtual;
+
+function mostraPerguntas{
+    perguntasAtual = pergunta(atual);
+    caixaPerguntas.textContent = perguntasAtual.enunciado;
+
+}
+
+    mostraPerguntas();
 
 
